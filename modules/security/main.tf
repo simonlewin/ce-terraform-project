c4602d6986925b99d6043b96347746d21c1899b8
Simon Lewin <37666516+simonlewin@users.noreply.github.com>
2024-03-04 13:59:43 +0000

    feat: add security group to allow https
    add security group and ingress/egress rules to allow https traffic

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -1,3 +1,4 @@
+# Security group to allow HTTP
 resource "aws_security_group" "allow_http" {
   name        = "allow_http"
   description = "Allow HTTP inbound and outbound traffic"
@@ -9,6 +10,7 @@ resource "aws_security_group" "allow_http" {
   }
 }
 
+# VPC security group ingress rule
 resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   security_group_id = aws_security_group.allow_http.id
 
@@ -22,6 +24,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   }
 }
 
+# VPC security group egress rule
 resource "aws_vpc_security_group_egress_rule" "allow_http" {
   security_group_id = aws_security_group.allow_http.id
 
@@ -29,6 +32,46 @@ resource "aws_vpc_security_group_egress_rule" "allow_http" {
   from_port   = 80
   ip_protocol = "tcp"
   to_port     = 80
+
+  tags = {
+    Name = "${var.name}-egress-rule"
+  }
+}
+
+# Security group to allow HTTPS
+resource "aws_security_group" "allow_https" {
+  name        = "allow_https"
+  description = "Allow HTTPS inbound and outbound traffic"
+
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name = "${var.name}-sg"
+  }
+}
+
+# VPC security group ingress rule
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+  security_group_id = aws_security_group.allow_https.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 443
+  ip_protocol = "tcp"
+  to_port     = 443
+
+  tags = {
+    Name = "${var.name}-ingress-rule"
+  }
+}
+
+# VPC security group egress rule
+resource "aws_vpc_security_group_egress_rule" "allow_https" {
+  security_group_id = aws_security_group.allow_https.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 443
+  ip_protocol = "tcp"
+  to_port     = 443
 
   tags = {
     Name = "${var.name}-egress-rule"
