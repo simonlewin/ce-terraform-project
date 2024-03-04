432cc3e1f395f3875668c52ca21a15cff6b8f8cc
Simon Lewin <37666516+simonlewin@users.noreply.github.com>
2024-03-04 11:27:01 +0000

    feat: add public subnet routing table and route assocaition
    add public routing table and rt association for each public subnet

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -41,3 +41,21 @@ resource "aws_internet_gateway" "igw" {
     Name = "${var.name}-igw"
   }
 }
+
+# Routing Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# Association between route table and public subnets
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnets)
+
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.public.id
+}
