13040ef7f540a0cac9a8fd08ccac33488854dd9b
Simon Lewin <37666516+simonlewin@users.noreply.github.com>
2024-03-05 16:50:01 +0000

    fix: add nat gateway to public subnet
    fix the subnet id in which to place the gateway

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -92,8 +92,8 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.vpc.id
 
   route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.natgw.id
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.natgw.id
   }
 
   tags = {
