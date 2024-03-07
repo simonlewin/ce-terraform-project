275d4cf3e0322c66fceb073f306e762ee82b7d8d
Simon Lewin <37666516+simonlewin@users.noreply.github.com>
2024-03-07 16:12:40 +0000

    fix: update list of IDs of instances
    combine list of IDs of the instances into one list

--- modules/servers/outputs.tf
+++ modules/servers/outputs.tf
@@ -1,9 +1,4 @@
-output "service_ids" {
-  value       = [aws_instance.lighting.id, aws_instance.heating.id, aws_instance.status.id]
-  description = "A list of IDs of the instances"
-}
-
-output "int_service_ids" {
-  value       = [aws_instance.lighting.id, aws_instance.heating.id, aws_instance.auth.id]
+output "instance_ids" {
+  value       = [aws_instance.lighting.id, aws_instance.heating.id, aws_instance.status.id, aws_instance.auth.id]
   description = "A list of IDs of the instances"
 }
