d005564df2612f0720bb6bf24d30232fe36cf308
Simon Lewin <37666516+simonlewin@users.noreply.github.com>
2024-03-07 09:01:14 +0000

    fix: update user data script
    update instance user data used at launch

--- modules/servers/main.tf
+++ modules/servers/main.tf
@@ -14,12 +14,18 @@ resource "aws_instance" "lighting" {
   }
 
   user_data = <<-EOT
-                #!/bin/bash
-                cd /home/ubuntu
-                git clone https://${var.GITHUB_TOKEN}@github.com/northcoders/ce-project-lights.git
-                sudo chown -R ubuntu:ubuntu ce-project-lights/
-                cd ce-project-lights
-                echo -e "ACCESS_KEY=${var.ACCESS_KEY}\nSECRET_ACCESS_KEY=${var.SECRET_ACCESS_KEY}\nTABLE_NAME=lighting" > .env.local
+                  !/bin/bash
+                  sudo -u ubuntu -i <<'EOF'
+                  cd /home/ubuntu
+                  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+                  . ~/.nvm/nvm.sh 
+                  nvm install --lts
+                  git clone https://${var.GITHUB_TOKEN}@github.com/northcoders/ce-project-lights.git
+                  cd ce-project-lights
+                  echo -e "ACCESS_KEY=${var.ACCESS_KEY}\nSECRET_ACCESS_KEY=${var.SECRET_ACCESS_KEY}\nTABLE_NAME=lighting" > .env.local
+                  npm install
+                  npm install pm2@latest -g
+                  EOF
                 EOT
 }
 
@@ -38,12 +44,45 @@ resource "aws_instance" "heating" {
   }
 
   user_data = <<-EOT
-                #!/bin/bash
-                cd /home/ubuntu
-                git clone https://${var.GITHUB_TOKEN}@github.com/northcoders/ce-project-heating.git
-                sudo chown -R ubuntu:ubuntu ce-project-heating/
-                cd ce-project-heating
-                echo -e "ACCESS_KEY=${var.ACCESS_KEY}\nSECRET_ACCESS_KEY=${var.SECRET_ACCESS_KEY}\nTABLE_NAME=heating" > .env.local
+                  !/bin/bash
+                  sudo -u ubuntu -i <<'EOF'
+                  cd /home/ubuntu
+                  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+                  . ~/.nvm/nvm.sh 
+                  nvm install --lts
+                  git clone https://${var.GITHUB_TOKEN}@github.com/northcoders/ce-project-heating.git
+                  cd ce-project-heating
+                  echo -e "ACCESS_KEY=${var.ACCESS_KEY}\nSECRET_ACCESS_KEY=${var.SECRET_ACCESS_KEY}\nTABLE_NAME=heating" > .env.local
+                  npm install
+                  npm install pm2@latest -g
+                  EOF
+                EOT
+}
+
+resource "aws_instance" "auth" {
+
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.private_security_group_ids
+  key_name               = var.key_name
+  subnet_id              = var.private_subnet_ids[0]
+
+  tags = {
+    Name = "${var.name}-auth"
+  }
+
+  user_data = <<-EOT
+                  !/bin/bash
+                  sudo -u ubuntu -i <<'EOF'
+                  cd /home/ubuntu
+                  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+                  . ~/.nvm/nvm.sh 
+                  nvm install --lts
+                  git clone https://${var.GITHUB_TOKEN}@github.com/northcoders/ce-smart-home-auth.git
+                  cd ce-smart-home-auth
+                  npm install
+                  npm install pm2@latest -g
+                  EOF
                 EOT
 }
 
@@ -62,32 +101,17 @@ resource "aws_instance" "status" {
   }
 
   user_data = <<-EOT
-                #!/bin/bash
-                cd /home/ubuntu
-                git clone https://${var.GITHUB_TOKEN}@github.com/northcoders/ce-project-status.git
-                sudo chown -R ubuntu:ubuntu ce-project-status/
-                cd ce-project-status
-                echo -e "LIGHTS_SERVICE=${aws_instance.lighting.private_ip}\nHEATING_SERVICE=${aws_instance.heating.private_ip}\nAUTH_SERVICE=${aws_instance.auth.private_ip}" > .env.local
-                EOT
-}
-
-resource "aws_instance" "auth" {
-
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  vpc_security_group_ids = var.private_security_group_ids
-  key_name               = var.key_name
-  subnet_id              = var.private_subnet_ids[0]
-
-  tags = {
-    Name = "${var.name}-auth"
-  }
-
-  user_data = <<-EOT
-                #!/bin/bash
-                cd /home/ubuntu
-                git clone https://${var.GITHUB_TOKEN}@github.com/northcoders/ce-smart-home-auth.git
-                sudo chown -R ubuntu:ubuntu ce-smart-home-auth/
-                cd ce-smart-home-auth
+                  !/bin/bash
+                  sudo -u ubuntu -i <<'EOF'
+                  cd /home/ubuntu
+                  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+                  . ~/.nvm/nvm.sh 
+                  nvm install --lts
+                  git clone https://${var.GITHUB_TOKEN}@github.com/northcoders/ce-project-status.git
+                  cd ce-project-status
+                  echo -e "LIGHTS_SERVICE=${aws_instance.lighting.private_ip}\nHEATING_SERVICE=${aws_instance.heating.private_ip}\nAUTH_SERVICE=${aws_instance.auth.private_ip}" > .env.local
+                  npm install
+                  npm install pm2@latest -g
+                  EOF
                 EOT
 }
